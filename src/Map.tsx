c49ce50bbe072ceb036547a1ee0a05d424a46466
ClanCo <corentin.terrie@gmail.com>
2024-02-14 18:26:30 +0100

    Fix tailwind config

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,7 +1,12 @@
 export default function Map() {
   return (
-    <div style={{ transform: 'translate3D(8px, 8px, 0px)' }}>
-      <div className=""></div>
+    <div className="relative" style={{ transform: 'translate3D(8px, 8px, 0px)' }}>
+      <div className="bg-blue-600" style={{ position: 'absolute', left: 'Opx', top: 'Opx' }}>
+        <canvas width="16px" height="16px" className="bg-blue-600" />
+      </div>
+      <div style={{ position: 'absolute', left: '16px', top: 'Opx' }}>
+        <canvas width="16px" height="16px" className="bg-blue-600" />
+      </div>
     </div>
   );
 }
